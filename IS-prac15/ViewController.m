32ccf65fbcd4b7cba358a17ce9f130a3517dd175
mits <mits.jp@gmail.com>
2014-01-26 12:11:29 +0900

    警告メッセージへの対応

--- IS-prac15/ViewController.m
+++ IS-prac15/ViewController.m
@@ -10,6 +10,9 @@
 
 @interface ViewController ()
 
+- (IBAction)prac15_3:(id)sender;
+- (IBAction)prac15_4:(id)sender;
+
 @end
 
 @implementation ViewController
@@ -24,6 +27,38 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+
+- (IBAction)prac15_3:(id)sender
+{
+    UIAlertView *alert =[[UIAlertView alloc] initWithTitle:@"選んでください"
+                                                   message:@"コース"
+                                                  delegate:self
+                                         cancelButtonTitle:@"キャンセル"
+                                         otherButtonTitles:@"Webアプリ開発Ⅰコース",@"Webアプリ開発Ⅱコース",@"iOSアプリ開発コース",nil];
+    [alert show];
+}
+
+- (IBAction)prac15_4:(id)sender
+{
+    UIActionSheet *sheet = [[UIActionSheet alloc] initWithTitle:@"コースを選んでください"
+                                                       delegate:self
+                                              cancelButtonTitle:@"キャンセル"
+                                         destructiveButtonTitle:@"iOSアプリ開発コース"
+                                              otherButtonTitles:@"Webアプリ開発Ⅰコース",@"Webアプリ開発Ⅱコース", nil];
+
+    [sheet showInView:self.view];
+}
+
+- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex
+{
+    NSLog(@"index=%d %@",buttonIndex,[alertView buttonTitleAtIndex:buttonIndex]);
+}
+
+-(void)actionSheet:(UIActionSheet *)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex
+{
+    NSLog(@"index=%d %@",buttonIndex,[actionSheet buttonTitleAtIndex:buttonIndex]);
 }
 
 @end
